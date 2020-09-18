ea0f912b7c8e38c39533483d550f3a20dad7bcd4
resetus <mkedevel@gmail.com>
2020-09-18 15:04:03 +0300

    добавлена корректная работа тестов jest, enzyme + react

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -2,7 +2,7 @@ import { hot } from 'react-hot-loader/root';
 import React from 'react';
 import styles from './header.less';
 
-const HeaderComponent = () => {
+export const HeaderComponent = () => {
   return (
     <header>
       <h1 className={styles.example}>Reddit for our own!!!</h1>
--- src/shared/example.test.js
+++ src/shared/example.test.js
@@ -1,3 +1,3 @@
 test('some', () => {
-	expect(true).toBe(true);
-})
+  expect(true).toBe(true);
+});
